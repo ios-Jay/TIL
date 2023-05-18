6d8fc8cecd6165e50bc1673bb86eabd08d1684be
ios-Jay <ios.developer.jay@gmail.com>
2023-05-18 16:51:47 +0900

    튜플의 활용

--- swift/섹션 8/4-2-tuple.playground/Contents.swift
+++ swift/섹션 8/4-2-tuple.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-// 강의 참조
+// 튜플의 활용
+
+// 튜플의 바인딩
+
+var coordinate = (0, 5)   // 좌표계
+
+switch coordinate {
+case (let distance, 0), (0, let distance):   // x축이나 y축에 있으면 출력하라는 코드
+    print("X 또는 Y축 위에 위치하며, \(distance)만큼의 거리가 떨어져 있음")
+default:
+    print("축 위에 있지 않음")
+}
+
+
+
+
+
+// 튜플의 where절 활용
+
+coordinate = (5, 0)
+
+
+switch coordinate {
+case (let x, let y) where x == y:      //일단 x, y를 상수로 지정해주고, 그리고 나서 비교한다.
+    print("(\(x), \(y))의 좌표는 y = x 1차함수의 그래프 위에 있다.")
+    
+case let (x, y) where x == -y:
+    print("(\(x), \(y))의 좌표는 y = -x 1차함수의 그래프 위에 있다.")
+    
+case let (x, y):
+    print("(\(x), \(y))의 좌표는 y = x, 또는 y = -x 그래프가 아닌 임의의 지점에 있다.")
+}
+
