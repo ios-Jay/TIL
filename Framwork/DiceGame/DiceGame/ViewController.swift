7591556fb4f2fec81f314d05e45b1e04074e3c58
ios-Jay <ios.developer.jay@gmail.com>
2024-05-09 18:59:14 +0900

    프레임워크 1주차

--- Framwork/DiceGame/DiceGame/ViewController.swift
+++ Framwork/DiceGame/DiceGame/ViewController.swift
@@ -1,8 +1,13 @@
-
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    @IBOutlet weak var myButton: UIButton!
+    
     
     @IBOutlet weak var firstImageView: UIImageView!
     
@@ -10,13 +15,17 @@ class ViewController: UIViewController {
     
     var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"),  #imageLiteral(resourceName: "black3"),  #imageLiteral(resourceName: "black4"),  #imageLiteral(resourceName: "black5"),  #imageLiteral(resourceName: "black6")]
     
+    // 앱을 처음 실행시킬때 처음 실행되는 함수
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        firstImageView.image = diceArray[2]
+        firstImageView.image = diceArray.randomElement()
         
-        secondImageView.image = diceArray[4]
+        secondImageView.image = diceArray.randomElement()
+        
+        mainLabel.textAlignment = NSTextAlignment.center
         
     }
 
@@ -26,6 +35,14 @@ class ViewController: UIViewController {
         firstImageView.image = diceArray.randomElement()
         
         secondImageView.image = diceArray.randomElement()
+        
+        mainLabel.textColor = .white
+        
+        mainLabel.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        
+        myButton.setTitleColor(.black, for: UIControl.State.normal)
+        
+        myButton.backgroundColor = .white
         
         
     }
