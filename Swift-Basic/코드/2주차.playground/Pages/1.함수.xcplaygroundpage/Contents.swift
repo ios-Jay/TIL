4e87cf920165e295c7a35f9196a7c4b0de0cfb56
ios-Jay <ios.developer.jay@gmail.com>
2024-05-10 18:16:10 +0900

    함수

---
+++ Swift-Basic/코드/2주차.playground/Pages/1.함수.xcplaygroundpage/Contents.swift
@@ -0,0 +1,144 @@
+import Foundation
+
+/*: ## 함수의 파라미터, 아규먼트
+ - 파라미터(Parameter)
+ ---
+ */
+/**=============================================================
+ 
+ - 파라미터: 함수를 정의시, 함수 정의에 입력값으로 사용되는 변수(내부사용)
+ 
+ - 아규먼트: 함수를 호출시, 함수가 필요한 파라미터의 타입과 일치하는 실제 값(외부사용)
+ 
+================================================================**/
+
+
+// 파라미터 이름만 사용할때(아규먼트 레이블을 사용 안하면)
+
+func printName(name: String) {
+    
+    // name: String -> 파라미터
+    
+    print("나의 이름은 \(name) 입니다.")
+}
+
+
+printName(name: "아인슈타인")    // 아인슈타인:아규먼트
+
+/*:---
+ - 1) 아규먼트 레이블(Argument Label)
+ ---
+ */
+// 아규먼트 레이블을 사용하면
+
+func printName1(first name: String) {
+    
+    print("나의 이름은 \(name) 입니다.")
+}
+
+// 외부에서 보여지는 것은 first(아규먼트 레이블),
+
+// 실제 내부에서 작동하는 것은 name(파라미터)
+
+printName1(first: "뉴턴")
+
+// 아규먼트 레이블을 사용 안하면
+
+func someFunction(first: Int, second: Int) {
+    
+    print(first + second)
+    
+}
+
+
+someFunction(first: 1, second: 2)
+
+
+// 아규먼트 레이블을 사용하는 것의 이점:일반적으로 함수를 사용할때 더 명확하게 무엇을 요구하는 지를 알려줄 수 있다.
+
+func someFunction1(writeYourFirstNumber a:Int,
+                   
+                   writeYourSecondNumber b: Int) {
+    
+    print(a + b)
+    
+}
+
+
+/*: ---
+ - 2) 아규먼트 레이블을 생략해서 사용하는 것도 가능 (와일드카드 패턴)
+ ---
+ */
+func addPrintFunction(_ firstNum: Int, _ secondNum: Int) {
+    
+    print(firstNum + secondNum)
+}
+
+// 와일드카드 패턴을 쓰는 이유:전달하고자 하는 바가 명확한 경우에 사용
+
+addPrintFunction(1, 2)
+
+/*:---
+ - 3) 가변 파라미터 -  함수의 파라미터에 정해지지 않은, 여러개의 값을 넣는 것도 가능(Variadic Parameters)
+ ---
+ */
+/**=============================================================
+ 
+ - 1) 하나의 파라미터로 2개 이상의 아규먼트를 전달할 수 있다.
+ 
+ - 2) 아규먼트는 배열형태로 전달된다.
+ 
+ - 3) 가변 파라미터는 개별함수마다 하나씩만 선언할 수있다.(선언 순서는 상관없음)
+ 
+ - 4) 가변 파라미터는 기본값을 가질 수 없다.
+ 
+===============================================================**/
+
+func arithMeticAverage(_ numbers: Double...) -> Double {
+    
+    var sum = 0.0
+    
+    for n in numbers {
+        
+        sum += n
+    }
+    
+    return sum / Double(numbers.count)
+}
+
+arithMeticAverage(3,3.6,7,6,8,9)
+
+
+/*: ---
+ - 4) 함수의 파라미터에 기본값(디폴트)을 정하는 것도 가능
+ ---
+ */
+func numFunction(num1: Int, num2: Int = 3) -> Int {
+    
+    var result = num1 + num2
+    
+    return result
+}
+
+// 함수를 실행할때 num2에 아규먼트를 주지 않는다면 기본값으로 3을 준다는 뜻
+
+
+numFunction(num1: 3)
+
+numFunction(num1: 3, num2: 7)
+
+
+
+//:> 아규먼트값이 항상 필요한 것이 아닐 수도 있다!
+
+// 실제 애플이 미리 만들어 놓은 함수에는 기본값이 들어 있는 경우가 꽤 있다.
+
+
+
+//print(<#T##items: Any...##Any#>, separator: <#T##String#>, terminator: <#T##String#>)
+
+
+
+// terminator: "\n" -> 엔터의 의미
+
+// separator: "" -> 글자를 연결 시킴. 띄어쓰기나 부호를 붙일 수 있음
