26db9a267414a28cd2bf7a22a69bd0d5fbfc3d74
ios-Jay <ios.developer.jay@gmail.com>
2024-05-22 13:57:47 +0900

    배열

--- Swift-Basic/코드/2주차.playground/Pages/3.컬렉션.xcplaygroundpage/Contents.swift
+++ Swift-Basic/코드/2주차.playground/Pages/3.컬렉션.xcplaygroundpage/Contents.swift
@@ -31,4 +31,57 @@ let numberArray = [1, 2, 3, 4, 5]
 
 var stringArray: [String] = []  // 빈배열: 타입을 반드시 명시해야 함
 
-stringArray = ["라민 야말", "페드리", "가비", "더용", ""]
+stringArray = ["라민 야말", "페드리", "가비", "더용", "슈테겐", "레반도프스키"]
+
+/*: ---
+ - 배열의 타입 표기
+ ---
+ */
+
+// 정식문법
+
+let strArray1: Array<Int> = []
+
+
+// 단축문법
+
+let strArray2: [String] = []
+
+/*: ---
+ - 빈 배열의 생성
+ ---
+ */
+
+/*
+ 빈 배열의 경우 타입을 지우면 컴파일러가 배열에 구체적으로 어떤 데이터를 담을 것인지를 알 수 없다. 따라서 빈 배열을 생성할 경우 타입을 반드시 선언해야 한다.
+ 
+ */
+
+let emptyArray1: [Int] = []
+
+let emptyArray2 = Array<Int>()
+
+// let emptyArray2: Array<Int> or [Int] = Array<Int>()   타입 선언
+
+let emptyArray3 = [Int]()
+
+print(emptyArray3)
+
+/*: ---
+ - 배열의 기본 기능
+ ---
+ */
+numsArray.count
+
+numsArray.isEmpty
+
+// !numsArray.isEmpty   // true
+
+numsArray.contains(1)  //파라미터로 값을 전달
+
+numsArray.contains(8)
+
+numsArray.randomElement()
+
+numsArray.swapAt(0, 1)
+
