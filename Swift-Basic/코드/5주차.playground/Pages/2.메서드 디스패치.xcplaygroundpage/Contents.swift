59a93760e90f3490a9d2c1ad534d5c17fd4a93f4
ios-Jay <ios.developer.jay@gmail.com>
2024-05-26 15:47:01 +0900

    6주차

--- Swift-Basic/코드/5주차.playground/Pages/2.메서드 디스패치.xcplaygroundpage/Contents.swift
+++ Swift-Basic/코드/5주차.playground/Pages/2.메서드 디스패치.xcplaygroundpage/Contents.swift
@@ -1,2 +1,24 @@
 import UIKit
 
+/*: # Method Dispatch
+ 
+ */
+
+/*: ## 메서드 디스패치(Method Dispatch)
+ - 1. 직접 디스패치 Direct Dispatch (Static)
+ ---
+ */
+struct MyStruct {
+    
+    func method1() { print("Struct - Direct method1") }
+    
+    func method2() { print("Struct - Direct method2") }
+    
+}
+
+
+let myStruct = MyStruct()
+
+myStruct.method1()
+
+myStruct.method2()
