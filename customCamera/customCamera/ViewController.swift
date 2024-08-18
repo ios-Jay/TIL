2ce4e1548ad78e88ca0a994c73335ef1d25608bf
ios-Jay <ios.developer.jay@gmail.com>
2024-08-18 14:01:32 +0900

    customCameraApp

---
+++ customCamera/customCamera/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  customCamera
+//
+//  Created by 김재혁 on 8/18/24.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        
+        
+        
+        
+        super.viewDidLoad()
+        
+        
+
+    }
+
+
+}
+
